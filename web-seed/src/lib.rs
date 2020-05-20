6c29ec4caa8ebf310a740957fe351cb68042290b
hageshtrem <hageshtrem@yahoo.com>
2020-05-20 16:20:32 +0300

    Bump seed version to 0.7, print table with result

--- web-seed/src/lib.rs
+++ web-seed/src/lib.rs
@@ -7,6 +7,8 @@ const API_URL: &str = env!("API_URL"); // "http://localhost/v1"
 struct Model {
     name: String,
     description: String,
+    emitents: Vec<Emitent>,
+    error: Option<String>,
 }
 
 impl Default for Model {
@@ -14,12 +16,20 @@ impl Default for Model {
         Self {
             name: String::new(),
             description: String::new(),
+            emitents: vec![],
+            error: None,
         }
     }
 }
 
+#[derive(Serialize, Clone)]
+struct NewEmitentRequest {
+    name: String,
+    description: String,
+}
+
 #[derive(Debug, Deserialize, Clone)]
-struct Reply {
+struct NewEmitentReply {
     message: String,
 }
 
@@ -28,79 +38,117 @@ struct GetEmitentsReply {
     results: Vec<Emitent>,
 }
 
-#[derive(Debug, Deserialize, Clone)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 struct Emitent {
     id: String, // https://developers.google.com/protocol-buffers/docs/proto3#json
     name: String,
     description: String,
 }
 
-#[derive(Clone)]
 enum Msg {
     NameChanged(String),
     DescriptionChanged(String),
     FetchData,
-    DataFetched(fetch::ResponseDataResult<GetEmitentsReply>),
+    DataFetched(fetch::Result<GetEmitentsReply>),
     Submit,
-    Submitted(fetch::ResponseDataResult<Reply>),
+    Submitted(fetch::Result<NewEmitentReply>),
 }
 
 fn update(msg: Msg, model: &mut Model, orders: &mut impl Orders<Msg>) {
     match msg {
         Msg::FetchData => {
-            log!("Fetch data");
-            orders.perform_cmd(async {
-                Request::new(format!("{}/emitent", API_URL))
-                    .fetch_json_data(Msg::DataFetched)
+            async fn do_request() -> fetch::Result<GetEmitentsReply> {
+                fetch(format!("{}/emitent", API_URL))
+                    .await?
+                    .check_status()?
+                    .json()
                     .await
-            });
+            }
+            orders.perform_cmd(async { Msg::DataFetched(do_request().await) });
         }
-        Msg::DataFetched(result) => log!(result),
+        Msg::DataFetched(Ok(ger)) => model.emitents = ger.results,
+        Msg::DataFetched(Err(error)) => log!(error),
         Msg::NameChanged(name) => model.name = name,
         Msg::DescriptionChanged(description) => model.description = description,
         Msg::Submit => {
-            log!("{}", API_URL);
-            let request = Request::new(format!("{}/emitent", API_URL))
-                .method(Method::Post)
-                .send_json(model);
-            orders.perform_cmd(async { request.fetch_json_data(Msg::Submitted).await });
+            let newemitent = NewEmitentRequest {
+                name: model.name.clone(),
+                description: model.description.clone(),
+            };
+            async fn send_message(req: NewEmitentRequest) -> fetch::Result<NewEmitentReply> {
+                Request::new(format!("{}/emitent", API_URL))
+                    .method(Method::Post)
+                    .json(&req)?
+                    .fetch()
+                    .await?
+                    .check_status()?
+                    .json()
+                    .await
+            }
+            orders.perform_cmd({ async { Msg::Submitted(send_message(newemitent).await) } });
         }
-        Msg::Submitted(Ok(response)) => log!(response),
+        Msg::Submitted(Ok(response)) => {
+            log!(response);
+            orders.send_msg(Msg::FetchData);
+        }
         Msg::Submitted(Err(error)) => {
-            log!("--------ERROR occured!-------");
             log!(error);
+            model.error = Some(format!("{:?}", error));
         }
     }
 }
 
-fn view(model: &Model) -> impl View<Msg> {
-    form![
+fn view(model: &Model) -> Node<Msg> {
+    let table_body = model
+        .emitents
+        .iter()
+        .map(|elem| {
+            tr![
+                td![elem.id.clone()],
+                td![elem.name.clone()],
+                td![elem.description.clone()],
+            ]
+        })
+        .collect::<Vec<Node<Msg>>>();
+    div![
         style! {
-            St::Display => "flex",
-            St::FlexDirection => "column",
+            St::Display => "grid"
+            St::GridTemplateColumns => "50% 50%"
         },
-        label!["Name"],
-        input![
-            attrs! {
-                At::Value => model.name
+        div![table![
+            tr![th!["#"], th!["Name"], th!["Description"],],
+            table_body,
+        ]],
+        div![
+            form![
+                label!["Name"],
+                input![
+                    attrs! {
+                        At::Value => model.name
+                    },
+                    input_ev(Ev::Input, Msg::NameChanged)
+                ],
+                label!["Description"],
+                input![
+                    attrs! {
+                        At::Value => model.description,
+                    },
+                    input_ev(Ev::Input, Msg::DescriptionChanged)
+                ],
+                button![
+                    "Submit",
+                    ev(Ev::Click, |event| {
+                        event.prevent_default();
+                        Msg::Submit
+                    })
+                ],
+            ],
+            if let Some(err_msg) = &model.error {
+                div![err_msg]
+            } else {
+                empty![]
             },
-            input_ev(Ev::Input, Msg::NameChanged)
         ],
-        label!["Description"],
-        input![
-            attrs! {
-                At::Value => model.description,
-            },
-            input_ev(Ev::Input, Msg::DescriptionChanged)
-        ],
-        button![
-            "Submit",
-            ev(Ev::Click, |event| {
-                event.prevent_default();
-                Msg::Submit
-            })
-        ],
-        "Note: Errors are logged into the console log.",
     ]
 }
 
